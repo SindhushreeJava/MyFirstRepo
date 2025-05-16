78709b414ce76cee90d35da11afba748a9f6e5c1
SindhushreeJava <sindhushree.hu@pivotree.com>
2025-05-16 17:14:02 +0530

    sec commit

--- Automation/src/Day1/DataType.java
+++ Automation/src/Day1/DataType.java
@@ -4,7 +4,8 @@ public class DataType {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the java");
-
+		System.out.println("Welcome to the class");
+		System.out.println("Welcome to the class");
 	}
 
 }
